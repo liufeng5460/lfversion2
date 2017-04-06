cbf83d5320d45b4b66e9555b98bdb689befb1b34
liufengDell <liufeng5460@163.com>
2017-04-06 19:47:07 +0800

    push pull test

--- build-lfversion-2-unknown-Debug/Tmp/main.cpp
+++ build-lfversion-2-unknown-Debug/Tmp/main.cpp
@@ -1,42 +1,14 @@
-#include "maininterface.h"
-#include <QApplication>
-#include <QDir>
+#include <iostream>
+#include <random>
 
+using namespace std;
 
-int main(int argc, char *argv[])
+int main()
 {
+    random_device rd;
+    mt19937_64 mt(rd());
+    for(int n = 0; n < 1000000; n++)
+        mt();
 
-    QApplication a(argc, argv);
-    MainInterface w;
-    w.show();
-
-    QDir *dirHandler=new QDir();
-    bool exist;
-
-    // 获取程序执行时的工作目录
-    QString currentDir=QCoreApplication::applicationDirPath();
-
-    // 如果 不存在 Key 子目录，就创建它
-    exist = dirHandler->exists(currentDir+"/Key");
-    if(!exist) dirHandler->mkdir(currentDir+"/Key");
-
-    // 如果 不存在 Tmp 子目录，就创建它
-    exist = dirHandler->exists(currentDir+"/Tmp");
-    if(!exist) dirHandler->mkdir(currentDir+"/Tmp");
-
-    // 如果 不存在 Key/AES 子目录，就创建它
-    exist = dirHandler->exists(currentDir+"/Key/AES");
-    if(!exist) dirHandler->mkdir(currentDir+"/Key/AES");
-
-    // 如果 不存在 Key/RSA 子目录，就创建它
-    exist = dirHandler->exists(currentDir+"/Key/RSA");
-    if(!exist) dirHandler->mkdir(currentDir+"/Key/RSA");
-
-
-//    qDebug()<<QCoreApplication::applicationDirPath();
-   // QCoreApplication::addLibraryPath("./lib");
-    //qDebug()<<QCoreApplication::libraryPaths();
-
-
-    return a.exec();
+    return 0;
 }
